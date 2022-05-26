a70b41e617bde491399afb7295f74fc5b15b0e99
brammie15 <Brammieverhulst@gmail.com>
2022-05-27 01:05:00 +0200

    s

--- src/counter.tsx
+++ src/counter.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal, onCleanup, onMount, Show } from "solid-js";
+import { Component, createSignal, onCleanup, onMount, Show, createEffect } from "solid-js"; //ima import ur mom
 import createWebsocket from "@solid-primitives/websocket";
 import { messageType } from "./messageType";
 import { Connect } from "vite";
@@ -10,24 +10,36 @@ import { Connect } from "vite";
 
 function Counter(){
     let statusCode = new Map([
-        [0, "Not Connected"],
-        [1, "Connected"],
-        [2, "Unknown"],
-        [3, "Not Connected"], ]); //DEV
+        [0, "CONNECTING"],
+        [1, "OPEN"],
+        [2, "CLOSING"],
+        [3, "CLOSED"], ]); //DEV
 
     const [connect, disconnect, send, state] = createWebsocket(
-      "ws://127.0.0.1:8080",
+      "ws://95.111.230.236:80",
       (msg: { data: any; }) => handleMessage(msg.data),
-      (msg) => handleError(msg),
+      (msg) => handleError(msg), //tbh i dont even handle it
       [],
       5,
       5000
     );
-
+    createEffect(() => {
+        console.log(state());
+        if(state() == 1){
+            setConnected(true);
+        }
+        if(state() == 3){
+            setConnected(false);
+        }
+    });
     let [count, setCount] = createSignal(0);
-    let [connected, setConnected] = createSignal(false);
-    
+    let [connected, setConnected] = createSignal(false); //do i even use this? 
     function handleClick(){
+        if(connected() != true){
+            console.log("Not connected to server :/")
+            return;
+        }
+        console.log("Sending Click")
         send("INC"); //fuck the server
     }
     function handleMessage(message : string){
@@ -36,6 +48,7 @@ function Counter(){
     }
     function connectLog(){
         connect();
+        console.log(state());
         if(state() == 1){
             console.log("Connected");
             setConnected(true);
@@ -59,7 +72,7 @@ function Counter(){
                 <Show when={state() == 0 || state() == 3} fallback={<></>}> 
                     <p class="text-large">Connecting...</p>
                 </Show>
-                <button disabled={connected()} class="bg-sky-600 rounded-xl p-2 pl-10 pr-10 mt-10 mx-3 hover:bg-sky-500 transition-colors disabled:bg-sky-900" onclick={handleClick}>Add</button>
+                <button disabled={false} class="bg-sky-600 rounded-xl p-2 pl-10 pr-10 mt-10 mx-3 hover:bg-sky-500 transition-colors disabled:bg-sky-900" onclick={handleClick}>Add</button>
                 {/* <button class="bg-red-600 rounded-xl p-2 pl-10 pr-10 mt-10 mx-3 hover:bg-red-500 transition-colors" onclick={connectLog}>Connect</button> */}
 
             </div>
